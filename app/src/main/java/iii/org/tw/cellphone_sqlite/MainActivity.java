7766ec85e9e804eb9cbf5216de2929507c1dd805
cf904d31 <cf904d31@gmail.com>
2016-09-21 12:24:58 +0800

    CellPhone_Sqlite

--- app/src/main/java/iii/org/tw/cellphone_sqlite/MainActivity.java
+++ app/src/main/java/iii/org/tw/cellphone_sqlite/MainActivity.java
@@ -1,5 +1,8 @@
 package iii.org.tw.cellphone_sqlite;
 
+import android.content.ContentValues;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -7,26 +10,62 @@ import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
     private TextView showtxt;
+    private MyDBHelper dbHelper;
+    private SQLiteDatabase db;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         showtxt = (TextView) findViewById(R.id.showtxt);
+
+        dbHelper = new MyDBHelper(this,"Abner",null,1);
+        db = dbHelper.getReadableDatabase();
     }
 
     public void Insert(View v) {
-
+        // INSERT INTO cust (cname,tel,birthday) VALUES ('brad','123','1999-01-02');
+        ContentValues data = new ContentValues();
+        data.put("cname", "Abner");
+        data.put("tel", "456");
+        data.put("birthday","2000-11-22");
+        db.insert("customer",null,data);
+        Search(null);
     }
 
     public void Search(View v) {
-
+//        //----- Select * From customer
+//        showtxt.setText("");
+//        Cursor c = db.query("customer",null,null,null,null,null,null);
+//        while (c.moveToNext()){
+//            String id = c.getString(c.getColumnIndex("id"));
+//            String cname = c.getString(c.getColumnIndex("cname"));
+//            String tel = c.getString(c.getColumnIndex("tel"));
+//            String birthday = c.getString(c.getColumnIndex("birthday"));
+//            showtxt.append(id+":"+cname+":"+tel+":"+birthday+"\n");
+//        }
+        //-----Select cname,tel,birthday From customer
+        showtxt.setText("");
+        Cursor c = db.query("customer",null,null,null,null,null,null);
+        while (c.moveToNext()){
+            String id = c.getString(c.getColumnIndex("id"));
+            String cname = c.getString(c.getColumnIndex("cname"));
+            String tel = c.getString(c.getColumnIndex("tel"));
+            String birthday = c.getString(c.getColumnIndex("birthday"));
+            showtxt.append(id+":"+cname+":"+tel+":"+birthday+"\n");
+        }
     }
 
     public void Update(View v) {
-
+        ContentValues data = new ContentValues();
+        data.put("cname","Kevin");
+        data.put("tel","789456");
+        db.update("customer",data,"id = ?",new String[]{"2"});
+        Search(null);
     }
 
     public void Delete(View v) {
-
+        //-----DELETE FROM customer WHERE id = 3 and cname = Abner;
+        db.delete("customer","id = ? and cname = ?",new String[]{"3","Abner"});
+        Search(null);
     }
 }
---
+++ app/src/main/java/iii/org/tw/cellphone_sqlite/MyDBHelper.java
@@ -0,0 +1,27 @@
+package iii.org.tw.cellphone_sqlite;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+/**
+ * Created by user on 2016/9/21.
+ */
+public class MyDBHelper extends SQLiteOpenHelper {
+    private final String createTabel =
+            "CREATE TABLE IF NOT EXISTS customer (id INTEGER PRIMARY KEY AUTOINCREMENT," +
+                    "cname TEXT,tel TEXT,birthday DATE)";
+    public MyDBHelper(Context context, String name, SQLiteDatabase.CursorFactory factory, int version) {
+        super(context, name, factory, version);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        db.execSQL(createTabel);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+
+    }
+}
